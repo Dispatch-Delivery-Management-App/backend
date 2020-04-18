5b9950cdcd66001e909affebc6eff9013fee8c17
Yujia Yan <sky@Yujias-MacBook-Pro.local>
2020-04-18 17:30:03 -0500

    change name

--- dispatcher/OrderDetail/serializers.py
+++ dispatcher/OrderDetail/serializers.py
@@ -4,4 +4,4 @@ from .models import *
 class OrderDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderDetail
-        fields = ('id','user', 'from_address', 'to_address', 'status', 'tracking', 'category', 'itemInfo', 'capacity', 'createTime',  'departTime', 'pickupTime', 'completeTime', 'station', 'shipping_method', 'shipping_number', 'total_cost', 'feedback')
+        fields = ('id','user', 'from_address', 'to_address', 'status', 'tracking', 'category', 'item_info', 'capacity', 'create_time',  'depart_time', 'pickup_time', 'complete_time', 'station', 'shipping_method', 'shipping_number', 'total_cost', 'feedback')
--- dispatcher/User/urls.py
+++ dispatcher/User/urls.py
@@ -4,5 +4,5 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename = "user")
-
+router.register(r'user-profile', ProfileViewSet, basename = "profile")
 urlpatterns = router.urls
