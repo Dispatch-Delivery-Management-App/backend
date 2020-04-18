a9e1fefe2100d780e60d8fa2b0831f0424d0ec6e
Na Li <nali@nichols-ve703-09898.apn.wlan.private.upenn.edu>
2020-04-18 00:31:40 -0400

    update address info in orderdetails

--- dispatcher/OrderDetail/utils.py
+++ dispatcher/OrderDetail/utils.py
@@ -1,5 +1,16 @@
+from django.db import connection
+
 status_dict = {
     "draft": 1,
     "incomplete": 2,
     "complete": 3
 }
+
+def executeSQL(sql):
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        columns = [col[0] for col in cursor.description]
+        return [
+            dict(zip(columns, row))
+            for row in cursor.fetchall()
+        ]
--- dispatcher/OrderDetail/views.py
+++ dispatcher/OrderDetail/views.py
@@ -1,6 +1,7 @@
 from .serializers import *
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from .utils import *
 
 class OrderDetailViewSet(viewsets.ModelViewSet):
     serializer_class = OrderDetailSerializer
@@ -11,13 +12,21 @@ class OrderDetailViewSet(viewsets.ModelViewSet):
     def create(self, request):
         user = request.data.get('userId', None)
         if user is not None:
-            queryset = OrderDetail.objects.filter(user=user)
+            # queryset = OrderDetail.objects.filter(user=user)
+            sql = "SELECT * FROM OrderDetail_orderdetail O "\
+                  "JOIN(SELECT id AS  from_id, firstname AS From_FirstName, lastname AS From_LastName, " \
+                  "street AS From_Street, city AS From_City, state AS From_State, zipcode AS From_Zipcode "\
+                  "FROM Address_address) A "\
+                  "ON O.from_address_id = A.from_id "\
+                  "JOIN(SELECT id AS to_id, firstname AS To_FirstName, lastname AS To_LastName, " \
+                  "street AS To_Street, city AS To_City, state AS To_State, zipcode As To_Zipcode " \
+                  "FROM Address_address) A2 " \
+                  "ON O.to_address_id = A2.to_id " \
+                  "WHERE O.user_id = {};".format(user)
+            res = executeSQL(sql)
         else:
-            return Response({"error": "Missing user id.", "status": 400}, status=status.HTTP_400_BAD_REQUEST)
-        queryset = queryset.values('id',"user","from_address","status","tracking","category",
-    "itemInfo", "capacity", "createTime", "departTime", "pickupTime", "completeTime",
-    "station", "shipping_method", "shipping_number", "total_cost")
-        return Response({"status": 200, "Response":{"order": queryset}}, status=status.HTTP_200_OK)
+            return Response({"status": 400, "error": "Missing user id."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"status": 200, "Response":{"order": res}}, status=status.HTTP_200_OK)
 
 class OrderListViewSet(viewsets.ModelViewSet):
     serializer_class = OrderDetailSerializer
@@ -28,11 +37,15 @@ class OrderListViewSet(viewsets.ModelViewSet):
     def create(self, request):
         user = request.data.get('userId', None)
         if user is not None:
-            queryset = OrderDetail.objects.filter(user=user)
+            sql = "SELECT OrderDetail_orderdetail.id, category, status, lastname " \
+                  "FROM OrderDetail_orderdetail " \
+                  "JOIN Address_address A2 ON OrderDetail_orderdetail.to_address_id = A2.id " \
+                  "WHERE OrderDetail_orderdetail.user_id = {};"\
+                .format(user)
+            res = executeSQL(sql)
         else:
             return Response({"error": "Missing user id.", "status": 400}, status=status.HTTP_400_BAD_REQUEST)
-        queryset = queryset.values('id', 'status')
-        return Response({"order": queryset, "status": 200}, status=status.HTTP_200_OK)
+        return Response({"order": res, "status": 200}, status=status.HTTP_200_OK)
 
 class PlaceOrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderDetailSerializer
