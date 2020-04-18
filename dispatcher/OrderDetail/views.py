1b1cb77ad322684afa444bd7ecf904900555f54f
Na Li <nali@nichols-ve703-09898.apn.wlan.private.upenn.edu>
2020-04-17 20:25:41 -0400

    orderdetail

--- dispatcher/OrderDetail/views.py
+++ dispatcher/OrderDetail/views.py
@@ -8,6 +8,15 @@ class OrderDetailViewSet(viewsets.ModelViewSet):
         queryset = OrderDetail.objects.all()
         return queryset
 
+    def create(self, request):
+        user = request.data.get('userId', None)
+        if user is not None:
+            queryset = OrderDetail.objects.filter(user=user)
+        else:
+            return Response({"error": "Missing user id.", "status": 400}, status=status.HTTP_400_BAD_REQUEST)
+        queryset = queryset.values('id', 'status')
+        return Response({"order": queryset, "status": 200}, status=status.HTTP_200_OK)
+
 class OrderListViewSet(viewsets.ModelViewSet):
     serializer_class = OrderDetailSerializer
     def get_queryset(self):
--- dispatcher/User/urls.py
+++ dispatcher/User/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import *
 from rest_framework.routers import DefaultRouter
+#
+# router = DefaultRouter()
+# router.register(r'users', UserViewSet, basename = "users")
+#
+# urlpatterns = router.urls
+
+from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename = "user")
