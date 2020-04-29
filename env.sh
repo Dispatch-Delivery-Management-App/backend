3104a314783a3e9cc79bdc0c2a0e1d1d6b7e84b4
Yujia Yan <sky@Yujias-MacBook-Pro.local>
2020-04-29 01:27:53 -0500

    update

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
-pip install django
-pip install djangorestframework
-pip install django-request
-pip install markdown
-pip install django-filter
-pip install psycopg2
-pip install PyMySQL
-pip install djangorestframework-firebase
+pip3 install django
+pip3 install djangorestframework
+pip3 install markdown
+pip3 install django-filter
+pip3 install psycopg2
+pip3 install PyMySQL
+pip3 install djangorestframework-firebase
+pip3 install dj-database-url gunicorn whitenoise
+pip3 install django-heroku
---
+++ env/lib/python3.7/site-packages/rest_framework_firebase/settings.py
@@ -0,0 +1,36 @@
+import datetime
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+
+USER_SETTINGS = getattr(settings, 'FIREBASE_AUTH', None)
+
+DEFAULTS = {
+    'FIREBASE_ACCOUNT_KEY_FILE': '', # JSON formatted key file you get directly from firebase
+    'FIREBASE_CREDENTIALS': {
+        'type': "service_account",
+        'project_id': "",
+        'private_key_id': "",
+        'private_key': "",
+        'client_email': "",
+        'client_id': "",
+        'auth_uri': "https://accounts.google.com/o/oauth2/auth",
+        'token_uri': "https://accounts.google.com/o/oauth2/token",
+        'auth_provider_x509_cert_url': "https://www.googleapis.com/oauth2/v1/certs",
+        'client_x509_cert_url': ""
+    }, # the credentials in raw json form
+    
+    'FIREBASE_CREATE_NEW_USER': True, # We'll make a new user if we get one that we don't have yet
+
+    'FIREBASE_AUTH_HEADER_PREFIX': "JWT",
+    'FIREBASE_UID_FIELD': 'username',
+    # Indicates if FB is set to allow one account per email (the default)
+    'FIREBASE_UNIQUE_EMAIL': True
+}
+
+# List of settings that may be in string import notation.
+IMPORT_STRINGS = (
+)
+
+api_settings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
