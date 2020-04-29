f75db029873106ee817c715cd1f9bcd75d3152d4
Yujia Yan <sky@Yujias-MacBook-Pro.local>
2020-04-29 15:14:32 -0500

    update address place order

--- env.sh
+++ env.sh
@@ -2,9 +2,10 @@ pip3 install django
 pip3 install djangorestframework
 pip3 install markdown
 pip3 install django-filter
-pip3 install psycopg2
 pip3 install PyMySQL
 pip3 install requests
 pip3 install djangorestframework-firebase
-pip3 install dj-database-url gunicorn whitenoise
-pip3 install django-heroku
+
+# pip3 install psycopg2
+# pip3 install dj-database-url gunicorn whitenoise
+# pip3 install django-heroku
